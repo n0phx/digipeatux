dfbfea3f56982a818b0d4f979e279a597e92260f
Andrean Franc <andrean.franc@gmail.com>
2019-01-17 15:27:56 +0100

    Add soundmodem package

---
+++ external/package/soundmodem/soundmodem.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# soundmodem
+#
+#################################################################################
+
+SOUNDMODEM_VERSION = 0.20
+SOUNDMODEM_SOURCE = soundmodem-$(SOUNDMODEM_VERSION).tar.gz
+SOUNDMODEM_SITE = https://archive.org/download/soundmodem-$(SOUNDMODEM_VERSION)
+SOUNDMODEM_LICENSE = GPL-2.0+
+SOUNDMODEM_LICENSE_FILES = COPYING
+SOUNDMODEM_INSTALL_STAGING = NO
+SOUNDMODEM_INSTALL_TARGET = YES
+SOUNDMODEM_AUTORECONF = YES
+SOUNDMODEM_DEPENDENCIES = alsa-lib audiofile libxml2
+
+define SOUNDMODEM_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D $(SOUNDMODEM_PKGDIR)/S50soundmodem \
+				$(TARGET_DIR)/etc/init.d/S50soundmodem
+endef
+
+$(eval $(autotools-package))
