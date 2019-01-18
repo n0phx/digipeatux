9f9071063d78e59ccfb9f1d20d498003a84b0ba1
Andrean Franc <andrean.franc@gmail.com>
2019-01-18 08:58:02 +0100

    Fix fstab patching so the same entry cannot be added multiple times

--- external/scripts/post-build.sh
+++ external/scripts/post-build.sh
@@ -10,13 +10,22 @@ DATA_DIR="${TARGET_DIR}/${DATA_PATH}"
 
 FSTAB="${TARGET_DIR}/etc/fstab"
 
+append_line() {
+  line="$1"
+  file="$2"
+  grep -qF -- "$line" "$file" || echo "$line" >> "$file"
+}
+
 # Make sure directories exist where the non-volatile partitions are mounted
 mkdir -p "$CONF_DIR"
 mkdir -p "$DATA_DIR"
 
 # Update fstab to mount the non-volatile partitions during boot
-echo "/dev/mmcblk0p3	${CONF_PATH}	ext4	defaults	0	1" >> $FSTAB
-echo "/dev/mmcblk0p4	${DATA_PATH}	ext4	defaults	0	1" >> $FSTAB
+CONF_ENTRY="/dev/mmcblk0p3	${CONF_PATH}	ext4	defaults	0	2"
+DATA_ENTRY="/dev/mmcblk0p4	${DATA_PATH}	ext4	defaults	0	2"
+
+append_line "$CONF_ENTRY" "$FSTAB"
+append_line "$DATA_ENTRY" "$FSTAB"
 
 # Disable tty0 serial console
 sed -i '/console 0/d' ${TARGET_DIR}/etc/inittab
