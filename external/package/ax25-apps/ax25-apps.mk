d84ec645ceac278831db49e325485acd6d829b61
Andrean Franc <andrean.franc@gmail.com>
2019-01-20 18:04:33 +0100

    Add ax25-apps package (ax25rtd's ax25_route and ip_route files seem fishy, I just hacked around the issue)

---
+++ external/package/ax25-apps/ax25-apps.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# ax25-apps
+#
+#################################################################################
+
+AX25_APPS_VERSION = 0.0.8-rc4
+AX25_APPS_SOURCE = ax25-apps-$(AX25_APPS_VERSION).tar.gz
+AX25_APPS_SITE = http://www.linux-ax25.org/pub/ax25-apps
+AX25_APPS_LICENSE = GPL-2
+AX25_APPS_LICENSE_FILES = COPYING
+AX25_APPS_INSTALL_STAGING = NO
+AX25_APPS_INSTALL_TARGET = YES
+AX25_APPS_DEPENDENCIES = libax25 ncurses
+
+define AX25_APPS_INSTALL_CONFIG
+# HACK: The following two files are touched / created here, because the Makefile
+# attempts to install them but fails as they don't exist actually..
+	touch $(@D)/ax25rtd/ax25_route
+	touch $(@D)/ax25rtd/ip_route
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) installconf
+endef
+
+AX25_APPS_POST_INSTALL_TARGET_HOOKS += AX25_APPS_INSTALL_CONFIG
+
+$(eval $(autotools-package))
