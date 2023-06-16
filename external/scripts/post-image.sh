980ed62c663493b2d81b6d15816c27132c7e18e9
Andrean Franc <andrean.franc@gmail.com>
2023-06-16 17:13:05 +0200

    Update post-image script based on changes in upstream post-image script

--- external/scripts/post-image.sh
+++ external/scripts/post-image.sh
@@ -30,48 +30,6 @@ getcmdline() {
 	grep "^CONFIG_CMDLINE=" "$path" | cut -d \" -f2
 }
 
-for arg in "$@"
-do
-	case "${arg}" in
-		--add-pi3-miniuart-bt-overlay)
-		if ! grep -qE '^dtoverlay=' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			echo "Adding 'dtoverlay=pi3-miniuart-bt' to config.txt (fixes ttyAMA0 serial console)."
-			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-
-# fixes rpi3 ttyAMA0 serial console
-dtoverlay=pi3-miniuart-bt
-__EOF__
-		fi
-		;;
-		--aarch64)
-		# Run a 64bits kernel (armv8)
-		sed -e '/^kernel=/s,=.*,=Image,' -i "${BINARIES_DIR}/rpi-firmware/config.txt"
-		if ! grep -qE '^arm_control=0x200' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-
-# enable 64bits support
-arm_control=0x200
-__EOF__
-		fi
-
-		# Enable uart console
-		if ! grep -qE '^enable_uart=1' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-
-# enable rpi3 ttyS0 serial console
-enable_uart=1
-__EOF__
-		fi
-		;;
-		--gpu_mem_256=*|--gpu_mem_512=*|--gpu_mem_1024=*)
-		# Set GPU memory
-		gpu_mem="${arg:2}"
-		sed -e "/^${gpu_mem%=*}=/s,=.*,=${gpu_mem##*=}," -i "${BINARIES_DIR}/rpi-firmware/config.txt"
-		;;
-	esac
-
-done
-
 # Enable audio on rpi
 AUDIO_ON="dtparam=audio=on"
 CONFIG_TXT="${BINARIES_DIR}/rpi-firmware/config.txt"
